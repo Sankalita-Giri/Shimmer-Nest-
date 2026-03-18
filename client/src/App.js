ed0bea1e93d2c9705e7085f36ec35c5299e550ac
Sankalita-Giri <sankalita371@gmail.com>
2026-03-18 13:44:36 +0530

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setActiveCategory(null);
     setSelectedProduct(null);
     setSearchTerm("");
-  };
+  };  
 
   // Logic for search results
   const searchedProducts = products.filter(p => 
